2b591ef06bc48de4ffb14d7ed57909a1263695fb
LauraToddCode <laura20todd@gmail.com>
2021-03-25 17:48:19 +0000

    Updated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# using-context
- Example code for an upcoming blog post about using Context in React
+# Using Context in React
+ Example code for a [blog post](https://www.lauratodddesign.com/blog/a-really-simple-intro-to-context-in-react/) about using Context in React.
+
+ I use a really simple example of passing the selected option from a dropdown menu to a sibling component using the React Context API.
+
+
--- src/Result.js
+++ src/Result.js
@@ -2,16 +2,16 @@ import React from "react";
 import { Context } from "./Context";
 
 function Result() {
-        return (
-            <div>
-                <h3>Result</h3>
-                <Context.Consumer>
-                    {(context) => (
-                        <p>{context.state.season}</p>
-                    )}
-                </Context.Consumer>
-            </div>
-        )
+    return (
+        <div>
+            <h3>Result</h3>
+            <Context.Consumer>
+                {(context) => (
+                    <p>{context.state.season}</p>
+                )}
+            </Context.Consumer>
+        </div>
+    )
 }
 
 export default Result;
